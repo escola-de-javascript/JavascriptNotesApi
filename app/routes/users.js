b32bba909d54f524004de02fca3d79981933179e
Leonardo Scorza <contato@escoladejavascript.com>
2019-11-09 12:52:57 -0300

    Aula - Autenticação - Registrando o usuário

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const User = require('../models/user');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post('/register', async(req, res) => {
+  const { name, email, password } = req.body;
+  const user = new User({ name, email, password });
+
+  try {
+    await user.save();
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({error: 'Error registering new user'});
+  }
+})
 
 module.exports = router;
