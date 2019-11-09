f1b2a34160e2124d5c2764333c5893c8679c189d
Leonardo Scorza <contato@escoladejavascript.com>
2019-11-09 18:38:16 -0300

    Aula - Atualizando uma nota

--- app/routes/notes.js
+++ app/routes/notes.js
@@ -37,6 +37,26 @@ router.get('/', withAuth, async(req, res) => {
   }
 })
 
+router.put('/:id', withAuth, async(req, res) => {
+  const { title, body } = req.body;
+  const { id } = req.params;
+
+  try {
+    let note = await Note.findById(id);
+    if(isOwner(req.user, note)){
+      let note = await Note.findOneAndUpdate(id, 
+        { $set: { title: title, body: body} },
+        { upsert: true, 'new': true }
+      );
+
+      res.json(note);
+    } else
+      res.status(403).json({error: 'Permission denied'});
+  } catch (error) {
+    res.status(500).json({error: 'Problem to update a note'});
+  }
+})
+
 const isOwner = (user, note) => {
   if(JSON.stringify(user._id) == JSON.stringify(note.author._id))
     return true;
