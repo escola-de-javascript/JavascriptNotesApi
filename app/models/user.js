d94affb9f43cb7ad3ce31b6e25c795e6ff4a9189
Leonardo Scorza <contato@escoladejavascript.com>
2019-11-09 16:51:17 -0300

    Aula - Autenticação - Permitindo o login do usuário

--- app/models/user.js
+++ app/models/user.js
@@ -24,4 +24,13 @@ userSchema.pre('save', function (next) {
   }
 });
 
+userSchema.methods.isCorrectPassword = function (password, callback) {
+  bcrypt.compare(password, this.password, function(err, same) {
+    if(err)
+      callback(err);
+    else
+      callback(err, same);
+  })
+}
+
 module.exports = mongoose.model('User', userSchema);
--- app/routes/users.js
+++ app/routes/users.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const secret = process.env.JWT_TOKEN;
 
 router.post('/register', async(req, res) => {
   const { name, email, password } = req.body;
@@ -11,6 +14,28 @@ router.post('/register', async(req, res) => {
     res.status(200).json(user);
   } catch (error) {
     res.status(500).json({error: 'Error registering new user'});
+  }
+})
+
+router.post('/login', async(req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+    if(!user)
+      res.status(401).json({error: 'Incorrect email or password'});
+    else {
+      user.isCorrectPassword(password, function(err, same){
+        if(!same)
+          res.status(401).json({error: 'Incorrect email or password'});
+        else {
+          const token = jwt.sign({email}, secret, { expiresIn: '10d' });
+          res.json({user: user, token: token});
+        }
+      })
+    }
+  } catch (error) {
+    res.status(500).json({error: 'Internal error, please try again'});
   }
 })
 
