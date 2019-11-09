f41583eed94e76c4d830460084075a397345bbf6
Leonardo Scorza <contato@escoladejavascript.com>
2019-11-09 18:43:26 -0300

    Aula - deletando uma nota

--- app/routes/notes.js
+++ app/routes/notes.js
@@ -57,6 +57,20 @@ router.put('/:id', withAuth, async(req, res) => {
   }
 })
 
+router.delete('/:id', withAuth, async(req, res) => {
+  const { id } = req.params;
+  try {
+    let note = await Note.findById(id);
+    if(isOwner(req.user, note)){
+      await note.delete();
+      res.json({message: 'OK'}).status(204);
+    } else
+      res.status(403).json({error: 'Permission denied'});
+  } catch (error) {
+    res.status(500).json({error: 'Problem to delete a note'});
+  }
+});
+
 const isOwner = (user, note) => {
   if(JSON.stringify(user._id) == JSON.stringify(note.author._id))
     return true;
