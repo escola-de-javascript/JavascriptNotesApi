a2064d69678a99a57662a7b5a87b4deb23dab612
Leonardo Scorza <contato@escoladejavascript.com>
2019-11-09 17:30:44 -0300

    Aula - Baixando uma nota

--- app/routes/notes.js
+++ app/routes/notes.js
@@ -15,4 +15,24 @@ router.post('/', withAuth, async (req, res) => {
   }
 })
 
+router.get('/:id', withAuth, async(req, res) =>{
+  try {
+    const { id } = req.params;
+    let note = await Note.findById(id);
+    if(isOwner(req.user, note))
+      res.json(note);
+    else
+      res.status(403).json({error: 'Permission denied'});
+  } catch (error) {
+    res.status(500).json({error: 'Problem to get a note'});
+  }
+})
+
+const isOwner = (user, note) => {
+  if(JSON.stringify(user._id) == JSON.stringify(note.author._id))
+    return true;
+  else
+    return false;
+}
+
 module.exports = router;
