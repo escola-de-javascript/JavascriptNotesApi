1c4ddf8dba69d3afcbc020a960f3a198647c3836
Leonardo Scorza <contato@escoladejavascript.com>
2019-11-09 17:35:47 -0300

    Aula - Baixando a lista de notas

--- app/routes/notes.js
+++ app/routes/notes.js
@@ -28,6 +28,15 @@ router.get('/:id', withAuth, async(req, res) =>{
   }
 })
 
+router.get('/', withAuth, async(req, res) => {
+  try {
+    let notes = await Note.find({author: req.user._id});
+    res.json(notes);
+  } catch (error) {
+    res.json({error: error}).status(500);
+  }
+})
+
 const isOwner = (user, note) => {
   if(JSON.stringify(user._id) == JSON.stringify(note.author._id))
     return true;
