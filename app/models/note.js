da3724feaac142b301d757acb4594c15850b0761
Leonardo Scorza <contato@escoladejavascript.com>
2019-11-09 12:11:20 -0300

    Aula - Criando a estrutura de dados

---
+++ app/models/note.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+let noteSchema = new mongoose.Schema({
+  title: String,
+  body: String,
+  created_at: { type: Date, default: Date.now },
+  updated_at: { type: Date, default: Date.now },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+})
+
+module.exports = mongoose.model('Note', noteSchema);
---
+++ app/models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+let userSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  created_at: { type: Date, default: Date.now },
+  updated_at: { type: Date, default: Date.now },
+});
+
+userSchema.pre('save', function (next) {
+  if (this.isNew || this.isModified('password')) {
+    bcrypt.hash(this.password, 10,
+      (err, hashedPassword) => {
+        if (err)
+          next(err)
+        else {
+          this.password = hashedPassword;
+          next();
+        }
+      }
+    )
+  }
+});
+
+module.exports = mongoose.model('User', userSchema);
