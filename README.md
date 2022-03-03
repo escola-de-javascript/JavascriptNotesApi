cd6c12babf845182c61c176f7a0791af412e8914
Gabriel Del <gabrieldelc.barros@gmail.com>
2022-03-02 22:58:09 -0300

    README

---
+++ README.md
@@ -0,0 +1,7 @@
+# Setup
+
+```bash
+systemctl start mongodb.service
+yarn install
+PORT=3001 yarn run start
+```
--- config/database.js
+++ config/database.js
@@ -3,7 +3,6 @@ mongoose.Promise = global.Promise;
 
 mongoose.connect('mongodb://localhost/javascriptNote', {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true
+  useUnifiedTopology: true
 }).then(() => console.log('Connection succesful'))
 .catch((err) => console.log(err));
