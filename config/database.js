67e8b3d37ddc942c87aab4b9e8fee9d983ba581e
Gabriel Del <gabrieldelc.barros@gmail.com>
2022-03-11 01:42:21 -0300

    dotenv

--- config/database.js
+++ config/database.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/javascriptNote', {
+require('dotenv').config();
+const MONGO_URL = process.env.MONGO_URL;
+mongoose.connect('mongodb://127.0.0.1/javascriptNote', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => console.log('Connection succesful'))
